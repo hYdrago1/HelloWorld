b595bed993dadb7cfd3910e1ea1e842af4ab021d
hYdrago1 <vanyagoncha@gmail.com>
2025-07-05 14:00:20 +0200

    Prepear Project. Delete templates comments

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,20 +1,13 @@
-//
-//  ViewController.swift
-//  HelloWorld
-//
-//  Created by Ivan Honcharov on 04.07.25.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
-//1:39
